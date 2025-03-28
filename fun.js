b67ddbc39a5c5567bd6776dfaccbd2c17d48b30f
Bhaisaniya07 <bhaisaniyavishal1@gmail.com>
2025-03-28 11:35:11 +0530

    done

---
+++ fun.js
@@ -0,0 +1,17 @@
+// function with parameter > velue1 , velue2
+
+function addtwonumber( velue1, velue2){
+
+    console.log('sum of:', velue1+ velue2)
+}
+
+
+addtwonumber(400,250);
+
+function abc(name){
+
+    console.log('your name is:',name);
+
+}
+
+abc("vishnu")
