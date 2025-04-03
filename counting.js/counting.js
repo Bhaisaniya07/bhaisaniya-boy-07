4a9261a4dece2a14bb8e4b1dc70b823fbc5b7191
Bhaisaniya07 <bhaisaniyavishal1@gmail.com>
2025-04-03 11:21:05 +0530

    done

---
+++ counting.js/counting.js
@@ -0,0 +1,11 @@
+// for loop 
+
+function PrintNumber(number){
+
+for(let i = 1; i<=number;i=(i+2)){
+
+    console.log('number:',i);
+}
+
+}
+PrintNumber(99);
---
+++ counting.js/fect.js
@@ -0,0 +1,9 @@
+let num = 5 ;
+
+let totel = 1 ;
+for(let i = 0 ; i<num; i++){
+
+    totel = totel*(num-i)
+};
+
+console.log(totel);
